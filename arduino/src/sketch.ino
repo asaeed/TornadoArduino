2a35a1b7ecebc9a9cf4e27ca349b2fed2911911b
Ahmad Saeed <asaeed@gmail.com>
2013-02-26 23:05:53 -0500

    simple arduino example

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,17 +1,42 @@
 
 #define LED_PIN 13
 
-void setup()
-{
+int msgByte= -1;         // incoming byte
+const int msgSize = 50;  // max message size
+char msgArray[msgSize];  // array for incoming data
+int msgPos = 0;          // current position
+
+void setup() {
     Serial.begin(115200);
     pinMode(LED_PIN, OUTPUT);
 }
 
-void loop()
-{
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("{'heading': 45}");
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(1900);
+void loop() {
+    handleSerial();
+}
+
+void handleSerial() {  
+  if (Serial.available() > 0) {
+  	digitalWrite(LED_PIN, HIGH);
+    msgByte = Serial.read();
+    
+    if (msgByte != '\n') {
+      // add incoming byte to array
+      msgArray[msgPos] = msgByte;
+      msgPos++;
+    } else {
+      // reached end of line
+      msgArray[msgPos] = 0;
+      Serial.print("message: "); Serial.println(msgArray);
+    
+      // reset byte array
+      for (int c = 0; c < msgSize; c++) 
+        msgArray[c] = ' ';
+
+      msgPos = 0;
+      digitalWrite(LED_PIN, LOW);
+    }
+  } else {
+    
+  }
 }
